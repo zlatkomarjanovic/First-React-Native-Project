d04daccd377ba3c2f7ea7c1927a675665d2c1f00
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2021-08-16 14:36:21 +0200

    Update ListScreem.js
    Update

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,17 +1,33 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
+
+
+
 const ListScreen = () => {
+
+    const friends = [
+        {name: "Friend #1"}, 
+        {name: "Friend #2"}, 
+        {name: "Friend #3"}, 
+        {name: "Friend #4"}, 
+        {name: "Friend #5"}, 
+        {name: "Friend #6"}, 
+        {name: "Friend #7"}, 
+        {name: "Friend #8"}, 
+        {name: "Friend #9"}, 
+    ]
     return (
-        <View>
-           <Text>
-                List Screen
-            </Text> 
-        </View>
+        <FlatList  
+            data = {friends} 
+            renderItem = {({item}) => {
+                return <Text>{item.name}</Text>
+        }}/>
+           
     )
 }
 
 const styles = StyleSheet.create({
-    
+
 })
 
 export default ListScreen
