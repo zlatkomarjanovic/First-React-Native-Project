0fe6b187a02d86f8ba3106c743d2c3f1287d5a84
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2021-08-17 15:44:44 +0200

    aa
    aa

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -5,10 +5,22 @@ import ImageDetail from '../components/ImageDetail';
 const ImageScreen = () => {
 	return (
 		<View>
-			<ImageDetail title='Forest' />
-			<ImageDetail title='Forest' />
-			<ImageDetail title='Forest' />
-			<ImageDetail title='Forest' />
+			<ImageDetail
+				imageSource={require('../../assets/forest.jpg')}
+				title='Forest'
+			/>
+			<ImageDetail
+				imageSource={require('../../assets/beach.jpg')}
+				title='Beach'
+			/>
+			<ImageDetail
+				imageSource={require('../../assets/mountain.jpg')}
+				title='Mountain'
+			/>
+			<ImageDetail
+				imageSource={require('../../assets/beach.jpg')}
+				title='Forest'
+			/>
 		</View>
 	);
 };
