9df1d0b4d376f231006bde7ca9b4de09c344de58
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2021-08-17 14:07:25 +0200

    Updates
    Updates

--- App.js
+++ App.js
@@ -5,17 +5,17 @@ import ComponentsScreen from './src/screens/ComponentsScreen';
 import ListScreen from './src/screens/ListScreen';
 
 const navigator = createStackNavigator(
-  {
-    Home: HomeScreen,
-    Components:ComponentsScreen, 
-    List: ListScreen
-  },
-  {
-    initialRouteName: 'List',
-    defaultNavigationOptions: {
-      title: 'App',
-    },
-  }
+	{
+		Home: HomeScreen,
+		Components: ComponentsScreen,
+		List: ListScreen,
+	},
+	{
+		initialRouteName: 'Home',
+		defaultNavigationOptions: {
+			title: 'App',
+		},
+	}
 );
 
 export default createAppContainer(navigator);
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,33 @@
-import React from "react";
-import { Text, StyleSheet } from "react-native";
+import React from 'react';
+import {
+	Text,
+	StyleSheet,
+	View,
+	Button,
+	TouchableOpacity,
+	Touchable,
+} from 'react-native';
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Kurchinaaa</Text>;
+const HomeScreen = (props) => {
+	console.log(props);
+	return (
+		<View>
+			<Text style={styles.text}>Kurchinaaa</Text>
+			<Button
+				title='Go to Components Demo'
+				onPress={() => props.navigation.navigate('Components')}
+			/>
+			<TouchableOpacity>
+				<Text>Go to List Demo</Text>
+			</TouchableOpacity>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 30
-  }
+	text: {
+		fontSize: 30,
+	},
 });
 
 export default HomeScreen;
